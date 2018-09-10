24e4ce0e1b52020929617a97a09a8a6d3e4ac077
Simon McFarland <10801276@my.uvu.edu>
2018-09-10 17:13:49 -0600

    added "hello world" text (and jump action?)

---
+++ dgm 1610 game simon/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	public int MoveSpeed;
+	public float Jumpheight = 15;
+
+	// Use this for initialization
+	void Start () {
+		print("hello world");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
