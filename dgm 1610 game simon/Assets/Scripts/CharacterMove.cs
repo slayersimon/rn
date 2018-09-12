6f3c926139551ab1daf8514a808cea9aee8452f8
Simon McFarland <10801276@my.uvu.edu>
2018-09-12 17:16:00 -0600

    Attempted to add controller movements

--- dgm 1610 game simon/Assets/Scripts/CharacterMove.cs
+++ dgm 1610 game simon/Assets/Scripts/CharacterMove.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
+	// playermovementvariables
 	public int MoveSpeed;
 	public float Jumpheight = 15;
+
+	//player grounded variables
+	public Transform Grundcheck;
+	public float Groundcheckrarius;
+	public LayerMask WhatIsGround;
+	private bool Grounded; 
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +20,21 @@ public class CharacterMove : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	Void FixedUpdate () (
+		Grounded = Physics2D;OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
+	// Update is called once per frame
+		void Update () (
+
+			//This code makes the character jump
+			if(Input.GetKeyDown (KeyCode.Space)&& Grounded)(
+				Jump();
+		
+		)
+		Public void Jump()(
+			GetComponent<Rigidbody2D>(.Velocity + new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight))
 	}
 }
