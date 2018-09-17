66924ab4f554e5e8d8d1a2facbeb7f20b6ac67e2
Simon McFarland <10801276@my.uvu.edu>
2018-09-17 17:19:58 -0600

    Fixed some movement code

--- dgm 1610 game simon/Assets/Scripts/CharacterMove.cs
+++ dgm 1610 game simon/Assets/Scripts/CharacterMove.cs
@@ -16,25 +16,38 @@ public class CharacterMove : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("hello world");
+
 	}
 	
+
+	void Fixupdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatsIsGround);
+	}
+
 	// Update is called once per frame
-	void Update () 
-	{
+	void Update () {}
 		
-	Void FixedUpdate () (
+	void FixedUpdate () {
 		Grounded = Physics2D;OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
 	}
 	// Update is called once per frame
-		void Update () (
+		void Update () {
 
 			//This code makes the character jump
-			if(Input.GetKeyDown (KeyCode.Space)&& Grounded)(
+			if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 				Jump();
-		
-		)
-		Public void Jump()(
-			GetComponent<Rigidbody2D>(.Velocity + new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight))
+			}
+			
+			//this code makes the character move from side to side using the A&D keys
+			if(Input.GetKey (KeyCode.D)){
+				GetComponent<Rigidbody2D>().Velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+			}
+
+		}
+	
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
