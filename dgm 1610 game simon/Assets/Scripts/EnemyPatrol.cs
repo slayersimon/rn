cedde04ebec0d09818f3dcdf6dc84dd29b230d14
Simon McFarland <10801276@my.uvu.edu>
2018-10-15 17:12:43 -0600

    Added enemy with walk cycle

---
+++ dgm 1610 game simon/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	//movement variables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	//wall check
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+
+	//Edge Check
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+
+	
+	// Update is called once per frame
+	void Update () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+		//turn around at wall or edge
+		if (HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-0.5f,0.5f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+		else {
+			transform.localScale = new Vector3(0.5f,0.5f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
