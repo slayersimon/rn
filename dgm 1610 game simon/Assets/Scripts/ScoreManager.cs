d710a6caa2a2e0b3a82bafe69f672555e5ac49df
Simon McFarland <10801276@my.uvu.edu>
2018-10-22 17:12:37 -0600

    Added ability to shoot!

--- dgm 1610 game simon/Assets/Scripts/ScoreManager.cs
+++ dgm 1610 game simon/Assets/Scripts/ScoreManager.cs
@@ -23,7 +23,9 @@ public class ScoreManager : MonoBehaviour {
 	}
 
 	public static void AddPoints (int PointsToAdd){
+	
 		Score += PointsToAdd;
+		
 	}
 
 	//public static void reset () {
---
+++ dgm 1610 game simon/Assets/Scripts/Shoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	//Shoot variables
+	public Transform FirePoint;
+	public GameObject Projectile;
+
+	void Start(){
+		Projectile = GameObject.Find("Projectile");
+
+	}
+	//update is called once per frame
+	void Update (){
+		if(Input.GetKeyDown(KeyCode.RightControl))
+		Instantiate(Projectile,FirePoint.position, FirePoint.rotation);
+
+	}
+}
