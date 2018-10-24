d01251da67cc963fff8cb65a4e3ce191eb9071e8
Simon McFarland <10801276@my.uvu.edu>
2018-10-24 17:04:19 -0600

    Created "While" statements

--- dgm 1610 game simon/Assets/Scripts/Loops.cs
+++ dgm 1610 game simon/Assets/Scripts/Loops.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour {
 
+	public int Num = 40;
+	public int Cat = 0;
+
+
 	// Use this for initialization
 	void Start () {
-		for(int i=0; i <= 100; i++){
+	// 	for(int i=0; i <= 100; i++){
 
-			print(i + "Ants Marching");
-		}
-		
+	// 		print(i + "Ants Marching");
+	// 	}
+
+	while(Num >= 0){
+		print("CountDown " + Num);
+		Num --;
+	}
+	while(Cat >= 0){
+		print("There's cat " + Cat);
+		Cat ++;
+	}
 	}
 	
 	// Update is called once per frame
