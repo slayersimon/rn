3568eef9bced2e1340aa36aabfdc0652b0519c53
Simon McFarland <10801276@my.uvu.edu>
2018-11-05 17:10:49 -0700

    Made camera follow player smoothly

---
+++ dgm 1610 game simon/Assets/Scripts/LoopsBrother.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopsBrother : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for(int i=0; 1 <= 100; i++){
+			print(i + )
+		}
+		
+	}
+	
+
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
