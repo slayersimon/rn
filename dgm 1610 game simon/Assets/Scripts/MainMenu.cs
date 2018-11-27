a74af9fccaccbb7230b439f8e22c63b80c4f1fc1
Simon McFarland <10801276@my.uvu.edu>
2018-11-26 17:18:36 -0700

    Made main menu script

---
+++ dgm 1610 game simon/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public int levelToLoad;
+	public void Loadlevel(){
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	public void LevelExit(){
+		Application.Quit();
+	}
+}
