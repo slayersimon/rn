ce9745913fe075901190672eefd13f93049633cc
Simon McFarland <10801276@my.uvu.edu>
2018-11-28 17:10:45 -0700

    added win screen ans escape button

--- dgm 1610 game simon/Assets/Scripts/ScoreManager.cs
+++ dgm 1610 game simon/Assets/Scripts/ScoreManager.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour {
+
 	public static int Score;
+	public int WinScore;
+
+	public Text WinText;
 
 	Text ScoreText;
 
+	void Awake(){
+		Time.timeScale = 1;
+	}
 	// Use this for initialization 
 	void Start () {
 		ScoreText = GetComponent<Text>();
 
 		Score = 0;
+
+		WinText.GetComponent<Text>().enabled = false;
 	}
 
 	// Update is called once per frame
@@ -20,6 +30,17 @@ public class ScoreManager : MonoBehaviour {
 		if (Score < 0)
 			Score = 0;
 		ScoreText.text = " " + Score;
+
+		//if the player wins display win text
+		if(Score >= WinScore ){
+			print("Win Score Reached = " + Score);
+			WinText.GetComponent<Text>().enabled = true;
+			Time.timeScale = 0;
+		}
+		//if player hits the escape key return to start menu
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene(0);
+		}
 	}
 
 	public static void AddPoints (int PointsToAdd){
