dc682ba76ef810ea565c61ecc9bc31da975bd113
Simon McFarland <10801276@my.uvu.edu>
2018-12-03 17:12:03 -0700

    Started health script

---
+++ dgm 1610 game simon/Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Health : MonoBehaviour {
+
+
+// 	public static int Health
+
+// 	// Use this for initialization
+// 	// void Start () {
+// 	// 		Health = GetComponent<Text>();
+
+// 	// 	Health = 3;
+		
+// 	// }
+	
+// 	// Update is called once per frame
+// // 	void Update () {
+	
+
+		
+// // 	}
+// }
